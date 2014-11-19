dd562e40362cf1f06584fdf1584b4f1c75c64436
Mattias Petter Johansson <mpj@spotify.com>
2014-11-19 22:40:19 +0100

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express();
 
+var mongodb = require('mongodb')
+, MongoClient = mongodb.MongoClient;
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
@@ -10,10 +13,30 @@ app.get('/', function(request, response) {
 
   if(subDomain.length > 2){
     subDomain = subDomain[0].split("-").join(" ");
+    MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
+      // operate on the collection named "test"
+      var collection = db.collection('people')
+      collection.findOne({
+        name: subDomain
+      }, function(err, item) {
+        if (!item) {
+          response.send('Hello World!'+item.isDead)
+        }
+        else {
+          response.send('Did not find that item')
+        }
+
+      })
+    })
   }else{
     subDomain = "Everyone ";
+    response.send('Hello World!'+subDomain)
   }
-  response.send('Hello World!'+subDomain)
+
+
+
+
+
 })
 
 app.listen(app.get('port'), function() {
